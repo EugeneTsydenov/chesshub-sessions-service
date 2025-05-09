840f04a31148dc8a451b5ca0399cb975ca4ab026
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 18:06:28 +0800

    feat: create base create session use case interface and implementation

---
+++ internal/app/usecase/create_session_usecase.go
@@ -0,0 +1,35 @@
+package usecase
+
+import (
+	"context"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/app/dto"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session/data/repo"
+	"log"
+)
+
+type CreateSessionUseCase interface {
+	Executor[*dto.CreateSessionInputDto, *dto.CreateSessionOutputDto]
+}
+
+type CreateSessionUseCaseImpl struct {
+	sessionsRepo repo.SessionsRepo
+}
+
+var _ CreateSessionUseCase = new(CreateSessionUseCaseImpl)
+
+func NewCreateSessionUseCase(sessionRepo repo.SessionsRepo) *CreateSessionUseCaseImpl {
+	return &CreateSessionUseCaseImpl{
+		sessionsRepo: sessionRepo,
+	}
+}
+
+func (u *CreateSessionUseCaseImpl) Execute(_ context.Context, input *dto.CreateSessionInputDto) (*dto.CreateSessionOutputDto, error) {
+	log.Print("CreateSessionUseCase Execute called")
+
+	_, _ = u.sessionsRepo.Create(context.Background(), nil)
+
+	return &dto.CreateSessionOutputDto{
+		SessionId: "sakhjdkajshfkasdjfhsdkj",
+		Message:   "Session created",
+	}, nil
+}
