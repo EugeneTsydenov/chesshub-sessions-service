3dc1197069814b91625b3a426c099a35aa63c5da
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 18:06:52 +0800

    feat: add create session use case to session service

--- internal/presentation/grpc/service/sessions_service.go
+++ internal/presentation/grpc/service/sessions_service.go
@@ -2,18 +2,27 @@ package service
 
 import (
 	"context"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/app/usecase"
 	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/presentation/grpc/generated/sessions"
+	"log"
 )
 
 type SessionsService struct {
 	sessions.UnimplementedSessionsServiceServer
+	createSessionUseCase usecase.CreateSessionUseCase
 }
 
-func NewSessionsService() *SessionsService {
-	return &SessionsService{}
+func NewSessionsService(createSessionUseCase usecase.CreateSessionUseCase) *SessionsService {
+	return &SessionsService{
+		createSessionUseCase: createSessionUseCase,
+	}
 }
 
 func (s *SessionsService) CreateSession(_ context.Context, _ *sessions.CreateSessionRequest) (*sessions.CreateSessionResponse, error) {
+	log.Print("CreateSession called")
+
+	_, _ = s.createSessionUseCase.Execute(context.Background(), nil)
+
 	return &sessions.CreateSessionResponse{
 		SessionId: "10",
 		Message:   "SUCCESS",
