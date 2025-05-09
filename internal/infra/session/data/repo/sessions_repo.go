38554cf9fbe376c5f5b0fce50383a8b3dee8d2ef
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 18:20:22 +0800

    feat: implement create session

--- internal/infra/session/data/repo/sessions_repo.go
+++ internal/infra/session/data/repo/sessions_repo.go
@@ -2,7 +2,9 @@ package repo
 
 import (
 	"context"
-	"log"
+	"errors"
+	"github.com/jackc/pgx/v5"
+	"time"
 
 	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session"
 	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session/data"
@@ -22,20 +24,66 @@ func NewSessionsRepo(pool data.DbPool) *SessionsRepoImpl {
 	return &SessionsRepoImpl{pool: pool}
 }
 
-func (s *SessionsRepoImpl) Create(_ context.Context, model *session.Session) (*session.Session, error) {
-	log.Print("CreateRepo Create called")
+func (r *SessionsRepoImpl) Create(ctx context.Context, model *session.Session) (*session.Session, error) {
+	query := `INSERT INTO sessions (user_id, ip_address, device_info, expired_at) 
+			  VALUES ($1, $2, $3, $4) 
+			  RETURNING id, user_id, ip_address, device_info, is_active, expired_at, updated_at, created_at`
 
-	return model, nil
+	row := r.pool.QueryRow(ctx, query, model.UserId(), model.IpAddr(), model.DeviceInfo(), model.ExpiredAt())
+
+	createdSession, err := scanSession(ctx, row)
+
+	if errors.Is(err, context.DeadlineExceeded) || errors.Is(err, context.Canceled) {
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return createdSession, nil
 }
 
-func (s *SessionsRepoImpl) Read(_ context.Context, spec any) (*session.Session, error) {
+func scanSession(ctx context.Context, row pgx.Row) (*session.Session, error) {
+	var (
+		id         string
+		userId     int64
+		ipAddr     string
+		deviceInfo string
+		isActive   bool
+		expiredAt  time.Time
+		updatedAt  time.Time
+		createdAt  time.Time
+	)
+
+	err := row.Scan(&id, &userId, &ipAddr, &deviceInfo, &isActive, &expiredAt, &updatedAt, &createdAt)
+	if err != nil {
+		return nil, err
+	}
+
+	b := session.NewBuilder()
+	s := b.
+		WithId(id).
+		WithUserId(userId).
+		WithIpAddr(ipAddr).
+		WithDeviceInfo(deviceInfo).
+		WithIsActive(isActive).
+		WithExpiredAt(expiredAt).
+		WithUpdatedAt(updatedAt).
+		WithCreatedAt(createdAt).
+		Build()
+
+	return s, nil
+}
+
+func (r *SessionsRepoImpl) Read(_ context.Context, spec any) (*session.Session, error) {
 	return nil, nil
 }
 
-func (s *SessionsRepoImpl) Update(_ context.Context, model *session.Session) (*session.Session, error) {
+func (r *SessionsRepoImpl) Update(_ context.Context, model *session.Session) (*session.Session, error) {
 	return model, nil
 }
 
-func (s *SessionsRepoImpl) Delete(_ context.Context, model *session.Session) (*session.Session, error) {
+func (r *SessionsRepoImpl) Delete(_ context.Context, model *session.Session) (*session.Session, error) {
 	return model, nil
 }
