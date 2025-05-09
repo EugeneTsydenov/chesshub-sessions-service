016670ee14dc9baf08c341cc4c9083a568c12906
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 15:28:01 +0800

    feat: add initial app, init deps and start app

--- cmd/sessions/main.go
+++ cmd/sessions/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "log"
+import (
+	"github.com/EugeneTsydenov/chesshub-sessions-service/app"
+	"log"
+)
 
 func main() {
-	log.Print("Running app")
+	a := app.New()
+
+	err := a.InitDeps()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Starting app...")
+	if err = a.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
