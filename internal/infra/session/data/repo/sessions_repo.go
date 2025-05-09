04df775130ca56e4ea3229379bf79f7e97baa884
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 18:05:28 +0800

    feat: create sessions repo interface and implementation

---
+++ internal/infra/session/data/repo/sessions_repo.go
@@ -0,0 +1,40 @@
+package repo
+
+import (
+	"context"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session/data"
+	"log"
+)
+
+type SessionsRepo interface {
+	repo[*session.Session]
+}
+
+type SessionsRepoImpl struct {
+	pool data.DbPool
+}
+
+var _ SessionsRepo = new(SessionsRepoImpl)
+
+func NewSessionsRepo(pool data.DbPool) *SessionsRepoImpl {
+	return &SessionsRepoImpl{pool: pool}
+}
+
+func (s *SessionsRepoImpl) Create(_ context.Context, model *session.Session) (*session.Session, error) {
+	log.Print("CreateRepo Create called")
+
+	return model, nil
+}
+
+func (s *SessionsRepoImpl) Read(_ context.Context, spec any) (*session.Session, error) {
+	return nil, nil
+}
+
+func (s *SessionsRepoImpl) Update(_ context.Context, model *session.Session) (*session.Session, error) {
+	return model, nil
+}
+
+func (s *SessionsRepoImpl) Delete(_ context.Context, model *session.Session) (*session.Session, error) {
+	return model, nil
+}
