2cf6b9bc6d001df2244344c09b92075f7c99234f
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 17:19:21 +0800

    feat: create inital migration where creating sessions table

---
+++ internal/infra/data/migrations/000001_initial.up.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE sessions (
+    id            uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id       int NOT NULL,
+    ip_address    varchar(45),
+    device_info   text,
+    expired_at    timestamp NOT NULL DEFAULT (now() + interval '30 days'),
+    created_at    timestamp DEFAULT now() NOT NULL,
+    updated_at    timestamp DEFAULT now() NOT NULL
+);
