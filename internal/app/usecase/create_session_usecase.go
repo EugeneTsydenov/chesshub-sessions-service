74b2bf33f56aa760f0f1a7d8d82c345cd14d3409
EugeneTsydenov <ecydenov110@gmail.com>
2025-05-09 18:55:39 +0800

    feat: implement base create session use case

--- internal/app/usecase/create_session_usecase.go
+++ internal/app/usecase/create_session_usecase.go
@@ -2,10 +2,13 @@ package usecase
 
 import (
 	"context"
-	"log"
-
+	"errors"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/app/apperrors"
 	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/app/dto"
+	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session"
 	"github.com/EugeneTsydenov/chesshub-sessions-service/internal/infra/session/data/repo"
+	"log"
+	"time"
 )
 
 type CreateSessionUseCase interface {
@@ -24,13 +27,37 @@ func NewCreateSessionUseCase(sessionRepo repo.SessionsRepo) *CreateSessionUseCas
 	}
 }
 
-func (u *CreateSessionUseCaseImpl) Execute(_ context.Context, input *dto.CreateSessionInputDto) (*dto.CreateSessionOutputDto, error) {
-	log.Print("CreateSessionUseCase Execute called")
+func (u *CreateSessionUseCaseImpl) Execute(ctx context.Context, input *dto.CreateSessionInputDto) (*dto.CreateSessionOutputDto, error) {
+	b := session.NewBuilder()
 
-	_, _ = u.sessionsRepo.Create(context.Background(), nil)
+	s := b.
+		WithUserId(input.UserId).
+		WithIpAddr(input.IpAddr).
+		WithDeviceInfo(input.DeviceInfo).
+		WithExpiredAt(input.ExpiredAt).
+		Build()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*2)
+	defer cancel()
+
+	createdSession, err := u.sessionsRepo.Create(ctx, s)
+
+	if errors.Is(err, context.DeadlineExceeded) {
+		log.Println("Context deadline exceeded: creation user to db too long(register use case)")
+		return nil, apperrors.NewDeadlineExceededError("user registration too long")
+	}
+
+	if errors.Is(err, context.Canceled) {
+		log.Println("Context cancelled: creation user closed(register use case)")
+		return nil, apperrors.NewCanceledError("user registration closed")
+	}
+
+	if err != nil {
+		return nil, apperrors.NewInternalError("Unexpected internal error")
+	}
 
 	return &dto.CreateSessionOutputDto{
-		SessionId: "sakhjdkajshfkasdjfhsdkj",
+		SessionId: createdSession.Id(),
 		Message:   "Session created",
 	}, nil
 }
